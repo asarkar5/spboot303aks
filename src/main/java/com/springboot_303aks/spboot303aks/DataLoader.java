eb82e7d93bb68c22ea177551aeca85e581d323b1
asarkar <asarkar@promnetwork.com>
2019-04-01 15:51:48 -0400

    Update for 304

---
+++ src/main/java/com/springboot_303aks/spboot303aks/DataLoader.java
@@ -0,0 +1,24 @@
+package com.springboot_303aks.spboot303aks;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner{
+
+    @Autowired
+    CourseRepository repository;
+
+    @Override
+    public void run(String... strings) throws Exception{
+        Course course = new Course("Astrophysics", "Neil D Tyson", "Just a course on stars", 3);
+          repository.save(course);
+        course = new Course("Calculas", "Carol Henly", "Rate of change of Rate of Change", 3);
+           repository.save(course);
+        course = new Course("Freshman English", "Geraldine Pegram", "Learn your language children", 3);
+        repository.save(course);
+
+    }
+}
